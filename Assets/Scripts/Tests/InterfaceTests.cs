fff71e544c0ce4a728be5beccfe6eca55127c221
Raysangar <raycosanchezgarcia@gmail.com>
2020-08-20 18:50:03 +0100

    Error conditions tested

--- Assets/Scripts/Tests/InterfaceTests.cs
+++ Assets/Scripts/Tests/InterfaceTests.cs
@@ -54,10 +54,54 @@ namespace Tests
             Assert.AreEqual(10, result, 0.001);
         }
 
+        [Test]
+        public void InterfaceReturnErrorIfInitializationFails()
+        {
+            bool errorCallbackCalled = false;
+            var backEndProviderMock = new Mock<IBackEndProvider>();
+            backEndProviderMock.Setup(c => c.RetrieveCurrencies(It.IsAny<Action<List<Currency>>>(), It.IsAny<Action<string>>()))
+                .Callback((Action<List<Currency>> onCurrenciesRetrieved, Action<string> errorCallback) => errorCallback(""));
+
+            var currencyExchanger = new CurrencyExchanger.CurrencyExchanger(backEndProviderMock.Object,
+                () => { },
+                (error) => { errorCallbackCalled = true; });
+
+            Assert.IsFalse(currencyExchanger.IsReady);
+            Assert.IsTrue(errorCallbackCalled);
+        }
+
+        [Test]
+        public void InterfaceReturnsErrorIfConversionFails()
+        {
+            bool errorCallbackCalled = false;
+            var backEndProviderMock = GetSuccesfullBackEndProvider();
+            backEndProviderMock.Setup(c => c.GetExchangeRateFor(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Action<float>>(), It.IsAny<Action<string>>()))
+                .Callback((string currencyFrom, string currencyTo, Action<float> currencyExchangeRetrievedCallback, Action<string> errorCallback) => errorCallback(""));
+
+            var currencyExchange = new CurrencyExchanger.CurrencyExchanger(backEndProviderMock.Object, () => { }, (error) => { });
+            currencyExchange.Convert("", "", 10, (value) => { }, (error) => { errorCallbackCalled = true; });
+
+            Assert.IsTrue(errorCallbackCalled);
+        }
+
+        [Test]
+        public void InterfaceReturnsErrorIfConversionWithDateFails()
+        {
+            bool errorCallbackCalled = false;
+            var backEndProviderMock = GetSuccesfullBackEndProvider();
+            backEndProviderMock.Setup(c => c.GetExchangeRateFor(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<Action<float>>(), It.IsAny<Action<string>>()))
+                .Callback((string currencyFrom, string currencyTo, DateTime date, Action<float> currencyExchangeRetrievedCallback, Action<string> errorCallback) => errorCallback(""));
+
+            var currencyExchange = new CurrencyExchanger.CurrencyExchanger(backEndProviderMock.Object, () => { }, (error) => {  });
+            currencyExchange.Convert("", "", 10, new DateTime(), (value) => { }, (error) => { errorCallbackCalled = true; });
+
+            Assert.IsTrue(errorCallbackCalled);
+        }
+
         private Mock<IBackEndProvider> GetSuccesfullBackEndProvider()
         {
             var backEndProviderMock = new Mock<IBackEndProvider>();
-            backEndProviderMock.Setup(c => c.RetrieveCurrencies(It.IsAny<Action<List<Currency>>>(), It.IsAny<System.Action<string>>()))
+            backEndProviderMock.Setup(c => c.RetrieveCurrencies(It.IsAny<Action<List<Currency>>>(), It.IsAny<Action<string>>()))
                 .Callback((Action<List<Currency>> onCurrenciesRetrieved, Action<string> errorCallback) => onCurrenciesRetrieved(new List<Currency>()));
             return backEndProviderMock;
         }
