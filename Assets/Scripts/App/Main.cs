301cc1a7c90175cc375485acb7e57cc06f2a22df
Raysangar <raycosanchezgarcia@gmail.com>
2020-08-20 19:06:39 +0100

    Playfab interface implemented

---
+++ Assets/Scripts/App/Main.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CurrencyExchanger
+{
+    public class Main : MonoBehaviour
+    {
+        void Awake()
+        {
+        }
+
+        private CurrencyExchanger currencyExchanger;
+    }
+}
---
+++ Assets/Scripts/App/PlayfabBackEndProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ServerModels;
+using UnityEngine;
+
+namespace CurrencyExchanger
+{
+    public class PlayfabBackEndProvider : IBackEndProvider
+    {
+        public void GetExchangeRateFor(string currencyFrom, string currencyTo, Action<float> excahngeRateRetrievedCallback, Action<string> errorCallback)
+        {
+            var request = new ExecuteCloudScriptServerRequest()
+            {
+                FunctionName = "GetExchangeRate",
+                FunctionParameter = new Dictionary<string, string>()
+                {
+                    { "from", currencyFrom },
+                    { "to", currencyTo },
+                }
+            };
+
+            PlayFabServerAPI.ExecuteCloudScript(request, (result) =>
+            {
+                excahngeRateRetrievedCallback((float) result.FunctionResult);
+            }, (error) => {
+                errorCallback(error.ErrorMessage);
+            });
+        }
+
+        public void GetExchangeRateFor(string currencyFrom, string currencyTo, DateTime date, Action<float> excahngeRateRetrievedCallback, Action<string> errorCallback)
+        {
+            var request = new ExecuteCloudScriptServerRequest()
+            {
+                FunctionName = "GetExchangeRate",
+                FunctionParameter = new Dictionary<string, string>()
+                {
+                    { "from", currencyFrom },
+                    { "to", currencyTo },
+                    { "date", date.ToShortDateString() }
+                }
+            };
+
+            PlayFabServerAPI.ExecuteCloudScript(request, (result) =>
+            {
+                excahngeRateRetrievedCallback((float)result.FunctionResult);
+            }, (error) => {
+                errorCallback(error.ErrorMessage);
+            });
+        }
+
+        public void RetrieveCurrencies(Action<List<Currency>> currenciesRetrievedCallback, Action<string> errorCallback)
+        {
+            var request = new ExecuteCloudScriptServerRequest()
+            {
+                FunctionName = "GetCurrencyList"
+            };
+
+            PlayFabServerAPI.ExecuteCloudScript(request, (result) =>
+            {
+                var list = JsonUtility.FromJson<List<Currency>>(result.FunctionResult.ToString());
+                currenciesRetrievedCallback(list);
+            }, (error) => {
+                errorCallback(error.ErrorMessage);
+            });
+        }
+    }
+}
